73f4d6d1936a5ecac5f0b596ebdfc796b382bffc
Oleg <oleg.futornoi@gmail.com>
2025-04-04 14:14:11 +0300

    fix admin panel

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -15,7 +15,7 @@ const AdminDashboard = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <Card>
             <CardHeader>
-              <CardTitle>XML Marketplace Connector</CardTitle>
+              <CardTitle>XMLMASTER</CardTitle>
               <CardDescription>Адміністративна панель</CardDescription>
             </CardHeader>
             <CardContent>
